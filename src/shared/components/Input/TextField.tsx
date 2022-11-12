c865a127d37614ef20565422138d245f113e3497
Alex Ilin <alexander.ilyin3@gmail.com>
2022-11-13 01:01:40 +0200

    chore: tailwind theme config

--- src/shared/components/Input/TextField.tsx
+++ src/shared/components/Input/TextField.tsx
@@ -7,5 +7,5 @@ import styles from './TextField.module.scss';
 type PropTypes = TextFieldProps;
 
 export const TextField: React.FC<PropTypes> = (props: PropTypes) => {
-  return <MuiTextField variant="outlined" className={`bg-red-500`} {...props}></MuiTextField>;
+  return <MuiTextField variant="outlined" className={`bg-primary-purple`} {...props}></MuiTextField>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      roboto: ['Roboto'],
+    },
+
+    extend: {
+      colors: {
+        primary: {
+          purple: '#857AAC',
+          dustPink: '#EEA5A3',
+        },
+        gray: {
+          500: '#EFEFEF',
+          600: '#CBCBCB',
+          700: '#757575',
+          800: '#5A5A5A',
+          900: '#222222',
+        },
+      },
+    },
   },
   plugins: [],
 };
