7711477e947158992c6d1e5c5c711c460c1bc214
Alex Ilin <alexander.ilyin3@gmail.com>
2022-11-12 21:31:59 +0200

    chore: mui installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
+import { ThemeProvider, CssBaseline, Input } from '@mui/material';
+
 import './App.css';
 import logo from './logo.svg';
+import theme from './theme';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Vidpovidno
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Input value={333}></Input>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Vidpovidno
+          </a>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
